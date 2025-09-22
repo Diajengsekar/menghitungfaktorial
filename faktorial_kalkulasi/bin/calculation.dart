d9c541cc8e35fdb43bfa24edc7276964c59e311f
Diajengsekar <diajengsekararum31@gmail.com>
2025-09-22 22:10:50 +0700

    Add factorial-calculation

--- faktorial_kalkulasi/bin/calculation.dart
+++ faktorial_kalkulasi/bin/calculation.dart
@@ -1,15 +1,19 @@
 import 'dart:io';
 
 void main() {
-  print("=== Program Faktorial (Versi Input Validation) ===");
+  print("=== Program Faktorial (Versi Perhitungan) ===");
 
   stdout.write("Masukkan bilangan bulat positif: ");
   String? input = stdin.readLineSync();
 
   int? angka = int.tryParse(input ?? "");
   if (angka == null || angka < 0) {
-    print("⚠️ Input tidak valid! Masukkan bilangan bulat positif.");
+    print(" Input tidak valid! Masukkan bilangan bulat positif.");
   } else {
-    print("Input valid: $angka");
+    int hasil = 1;
+    for (int i = 1; i <= angka; i++) {
+      hasil *= i;
+    }
+    print("Faktorial dari $angka adalah $hasil");
   }
 }
