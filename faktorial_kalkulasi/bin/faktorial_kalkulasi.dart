5cf799a24c75ab4fd24b7f9edcc7dadbbd9d34b7
Diajengsekar <diajengsekararum31@gmail.com>
2025-09-22 22:03:14 +0700

    add new project

---
+++ faktorial_kalkulasi/bin/faktorial_kalkulasi.dart
@@ -0,0 +1,5 @@
+import 'package:faktorial_kalkulasi/faktorial_kalkulasi.dart' as faktorial_kalkulasi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${faktorial_kalkulasi.calculate()}!');
+}
---
+++ faktorial_kalkulasi/test/faktorial_kalkulasi_test.dart
@@ -0,0 +1,8 @@
+import 'package:faktorial_kalkulasi/faktorial_kalkulasi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
